5f1ea79aeb9bf6376da4d2a4b3b875617e685e83
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-08-20 16:50:36 -0700

    Add react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 
 import './App.css';
+import Home from './component/Home.js';
+import Inventory from './component/Inventory.js';
+//Modules
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 //Components
 import Navigation from './component/Navigation.js';
@@ -8,6 +12,12 @@ function App() {
   return (
     <div className="App">
       <Navigation />
+      <BrowserRouter>
+        <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="inventory" element={<Inventory />} />
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
