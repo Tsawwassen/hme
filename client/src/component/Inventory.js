c54d3027cb1bd076e8bb2b57497e025fa8bdf5ae
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-08-26 16:38:51 -0700

    Add plan of action, and switch to change between upload and report views

--- client/src/component/Inventory.js
+++ client/src/component/Inventory.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+import Upload from './Upload';
+
+
+const  UPLOAD = 0;
+const  REPORT = 1;
 
 class Inventory extends Component {
   constructor(props){
     super(props);
     this.state = {
-     
+      expectedFilePath: {},
+      expectedFileData: [],
+      excpectedTable:[],
+      view: UPLOAD
     };
+
+    this.testButtonClicked = this.testButtonClicked.bind(this);
   }
+
+  //Button to test state values
+  testButtonClicked(){
+    console.log("Test Button clicked");
+    console.log(this.state.view);
+    this.setState({view: REPORT});
+    console.log(this.state.view);
+    console.log(this.state.excpectedTable);
+  }
+    //-----PLAN OF ACTION ----
     //-----V1----
     //Upload two different CSV files
     //Compare the two files (Both files will have part number and quantity columns)
@@ -27,10 +49,12 @@ class Inventory extends Component {
     //Scan items and create a list with part number and update quantity as they are scanned
     //Show report comparing the two files and give option to export report
     render() {
-      return (
+      return (<>
         <h1>Inventory Screen</h1>
-        
-      );
+        {this.state.view === UPLOAD && <Upload />}
+        {this.state.view === REPORT && <h2>REPORT</h2>}
+        <br /><Button variant="primary" type="button" onClick={this.testButtonClicked}>INVENTORY TEST</Button>
+        </>);
     }
   }
   
