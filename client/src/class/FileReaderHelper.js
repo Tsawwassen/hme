81515249383e0981af3f484e62b140cd48327967
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-08-27 07:17:32 -0700

    Add FileHeaderHelper class and class folder

---
+++ client/src/class/FileReaderHelper.js
@@ -0,0 +1,11 @@
+class FileReaderHelper {
+    static classTest = "Varialbe";
+
+    static classTestFunction(i){
+        console.log("inside function");
+        console.log(`Variable ${i}`);
+    }
+
+}
+
+export default FileReaderHelper;
