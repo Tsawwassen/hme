73c61f499ca319357cec641616d8c914a9be4363
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-09-03 07:48:37 -0700

    Add comments to Upload component

--- client/src/component/Upload.js
+++ client/src/component/Upload.js
@@ -1,10 +1,15 @@
+// React imports
 import React, { Component } from 'react';
 import { Col, Row, Form, Button } from 'react-bootstrap';
 
+// Helper Class
 import FileReaderHelper from '../class/FileReaderHelper';
 
+// Upload component
+// Render two file inputs, and submit button
 class Upload extends Component {
   
+    // Store two file paths and two file data as JSON array
     constructor(props){
         super(props);
         this.state = {
@@ -14,29 +19,36 @@ class Upload extends Component {
             actualFileData: []
         };
 
+        // OnChange functions
         this.expectedFileOnChange = this.expectedFileOnChange.bind(this);
         this.actualFileOnChange = this.actualFileOnChange.bind(this);
 
-        this.submit = this.submit.bind(this);
-        this.testButtonClicked = this.testButtonClicked.bind(this);
-        
+        // Set Functions
         this.setExpectedFileData = this.setExpectedFileData.bind(this);
         this.setActualFileData = this.setActualFileData.bind(this);
+
+        //Submit function
+        this.submit = this.submit.bind(this);
+
+        // Helper button to show state variables
+        this.testButtonClicked = this.testButtonClicked.bind(this);
     }
 
+    // Helper button to show state variables
     testButtonClicked(){  
         console.log(this.state.expectedFileData);
         console.log(this.state.actualFileData);
     }
 
+    // Set Functions
     setExpectedFileData(data){
         this.setState({expectedFileData: data});
     }
-
     setActualFileData(data){
         this.setState({actualFileData: data});
     }
 
+    // Submit input files paths to be parsed
     submit(){
        FileReaderHelper.getFileContent(this.state.expectedFilePath, this.setExpectedFileData, "Please select an expected inventory file");
        FileReaderHelper.getFileContent(this.state.actualFilePath, this.setActualFileData, "Please select an actual inventory file");
@@ -44,14 +56,15 @@ class Upload extends Component {
        //TODO : Get the FileData to parent component, and show data on screen
     }
 
+    // OnChange functions
     expectedFileOnChange(e){
         this.setState({expectedFilePath: e.target.files[0]});
     }
-
     actualFileOnChange(e){
         this.setState({actualFilePath: e.target.files[0]});
     }
     
+    // Render two file inputs and submit button.
     render() {
         return (<>
             <h2>Upload COMPONENT</h2>
@@ -59,16 +72,16 @@ class Upload extends Component {
                 <Col>
                 <Form>
                     <Form.Group controlId="formFile" className="mb-3">
-                        <Form.Label>Input CSV file</Form.Label>
+                        <Form.Label>Expected Inventory file</Form.Label>
                         <Form.Control type="file" onChange={this.expectedFileOnChange}  />
+                        <Form.Label>Actual Inventory file</Form.Label>
                         <Form.Control type="file" onChange={this.actualFileOnChange} />
                     </Form.Group>
                     <Button variant="primary" type="button" onClick={this.submit}>Submit</Button>
-                    
                 </Form>
                 </Col>
             </Row>
-            <Button variant="primary" type="button" onClick={this.testButtonClicked}>UPLOAD TEST</Button>
+            {/** Test Button to see component state variables */}<Button variant="primary" type="button" onClick={this.testButtonClicked}>UPLOAD TEST</Button>
         </>);
       };
     }
