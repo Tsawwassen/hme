d1720ba78b66582aaade935d0d85292a0e4ef227
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-10-08 14:18:52 -0700

    Change order module to be singular

---
+++ server/models/Order.js
@@ -0,0 +1,20 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+// Define a schema
+//const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  orderNumber: String,
+  rep: String,
+},
+{
+    collection: 'orders'
+});
+
+// Export function to create "SomeModel" model class
+module.exports = mongoose.model('Order', OrderSchema);
+//const Order = model('Order', OrderSchema);
+//export default Order;
