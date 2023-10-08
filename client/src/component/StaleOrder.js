de26a24b550ff0d7f2a40a2db954983698a80609
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-10-08 07:01:56 -0700

    Add StaleOrder function objective and notes

--- client/src/component/StaleOrder.js
+++ client/src/component/StaleOrder.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 
+
+/**
+ * Stale Order Tool
+ * - Scan order that are sitting on a shelf
+ * - Compare current list with previous list
+ * - If the order is new, save the order
+ * - If the order is in the previous list, keep the order
+ * - If the order is not in the new list, delete it (?)
+ * - Show a list of orders on the shelf, highlighting orders that have been sitting on the shelf from previous order audit
+ * -
+ */
+
+/**
+ * V1
+ * - Use input file to complete task
+ */
+
 class StaleOrder extends Component  {
     constructor(props){
       super(props);
