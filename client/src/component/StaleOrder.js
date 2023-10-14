64eb2387777c7c3ca6139af7c59199d6a116bd3e
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-10-14 08:50:16 -0700

    Remove duplicate orders from upload data

--- client/src/component/StaleOrder.js
+++ client/src/component/StaleOrder.js
@@ -10,13 +10,14 @@ import ViewOldOrders from './ViewOldOrders';
  * - If the order is new, save the order
  * - If the order is in the previous list, keep the order
  * - If the order is not in the new list, delete it (?)
+ * - - Maybe add a database column with a date of when an old order was not on the new order audit
  * - Show a list of orders on the shelf, highlighting orders that have been sitting on the shelf from previous order audit
  * -
  */
 
 /**
  * V1
- * - Use input file to complete task
+ * - Use input file to complete task - DONE
  */
 
 /**
@@ -24,12 +25,6 @@ import ViewOldOrders from './ViewOldOrders';
  * - Delete all data from database
  * - Parse given NEW order file (columns should be order number and rep)
  * - compare NEW orders with OLD orders (from database)
- * - - If NEW order.order_number is in OLD orders
- * - - - Add NEW order to return list
- * - - If NEW order.order_number is not in OLD orders
- * - - - Add NEW order to return list
- * - - If OLD order.order_number is not in NEW orders
- * - - - Delete from server
  */
 
 
@@ -41,9 +36,31 @@ class StaleOrder extends Component  {
       };
       
       this.setOrders = this.setOrders.bind(this);
+      this.removeDuplicates = this.removeDuplicates.bind(this);
     }
 
+    removeDuplicates(data){
+      return data.filter((data, index, self) =>
+        index === self.findIndex((t) => (t.orderNumber === data.orderNumber )))
+      
+    }
+    //Would rather pass the setState function to the UploadOldOrders component and not have a function to handle this
+    // // Not sure what way of doing it is good practise.
     setOrders(data){
+      /** Plan of action
+      * 1. check for duplicate order numbers
+      * 2. Sort data by rep, then order number
+      * 3. Compare orders with database and update as needed
+      * 3a.* - - If NEW order.order_number is in OLD orders
+      * - - - Add NEW order to return list
+      * - - If NEW order.order_number is not in OLD orders
+      * - - - Add NEW order to return list
+      * - - If OLD order.order_number is not in NEW orders
+      * - - - Delete from server
+      */
+      
+      data = this.removeDuplicates (data);
+      console.log(data);
       this.setState({orders: data});
     }
 
@@ -52,7 +69,6 @@ class StaleOrder extends Component  {
      * The thinking behind putting it in the UploadOldOrders component is that the component is getting a setState prop that would get used when a file is uploaded
     */
     componentDidMount(){
-      
       fetch("http://localhost:8080/orders")
       .then(response => {
         return response.json();
