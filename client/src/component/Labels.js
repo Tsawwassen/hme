c733013fc4967c041bc1652943742cf181cc6c78
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-05 07:55:31 -0800

    Wrap elements that should not be displayed when printing inside of a div tag with ID label-select

--- client/src/component/Labels.js
+++ client/src/component/Labels.js
@@ -26,17 +26,19 @@ class Labels extends Component {
   
     render() {
         return (<>
-            <h2>Labels</h2>
-            <Form>
-            <Form.Group className="mb-3" controlId="formLabelSelection">
-                <Form.Label>Select Label to Print</Form.Label>
-                <Form.Select aria-label="Default select example" onChange={this.labelOnChange}>
-                    <option value="0" >Select Label to Print</option>
-                    <option value="1" >Shelf Label</option>
-                    <option value="2">Inventory Label</option>
-                </Form.Select>
-            </Form.Group>
-            </Form>
+            <div id="label-select">
+                <h2>Labels</h2>
+                <Form>
+                <Form.Group className="mb-3" controlId="formLabelSelection">
+                    <Form.Label>Select Label to Print</Form.Label>
+                    <Form.Select aria-label="Default select example" onChange={this.labelOnChange}>
+                        <option value="0" >Select Label to Print</option>
+                        <option value="1" >Shelf Label</option>
+                        <option value="2">Inventory Label</option>
+                    </Form.Select>
+                </Form.Group>
+                </Form>
+            </div>
             {this.state.selectedLabelType === "0"}
             {this.state.selectedLabelType === "1" && <ShelfLabel />}
             {this.state.selectedLabelType === "2" && <h3>Inventory Label</h3>}
