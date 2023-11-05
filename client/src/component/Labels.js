53a8ef94a2d3239c049dc92b5eb92b3abaaad1c8
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-05 06:37:52 -0800

    Add select box form to select different label type, and change what component is rendered based on selected label type

--- client/src/component/Labels.js
+++ client/src/component/Labels.js
@@ -27,6 +27,22 @@ class Labels extends Component {
     render() {
         return (<>
             <h2>Labels</h2>
+            <Form>
+            <Form.Group className="mb-3" controlId="formLabelSelection">
+                <Form.Label>Select Label to Print</Form.Label>
+                <Form.Select aria-label="Default select example" onChange={this.labelOnChange}>
+                    <option value="0" >Select Label to Print</option>
+                    <option value="1" >Shelf Label</option>
+                    <option value="2">Inventory Label</option>
+                </Form.Select>
+            </Form.Group>
+            </Form>
+            {this.state.selectedLabelType === "0"}
+            {this.state.selectedLabelType === "1" && <h3>Shelf Label</h3>}
+            {this.state.selectedLabelType === "2" && <h3>Inventory Label</h3>}
+            <h3> test</h3>
+
+        
         </>);
       };
     }
