947ae47a4ee96195dfc1a956492f9ac983daf112
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-11 06:22:49 -0800

    Remove \r from parsed file data. moved todo note to the loops that need some work

--- client/src/component/ShelfLabel.js
+++ client/src/component/ShelfLabel.js
@@ -27,16 +27,23 @@ class ShelfLabel extends Component {
   }
 
   handleFileRead(e)  {
-    /**
-     * TODO : this code can probably be cleaned up A LOT, but it is working
-     *      The first CSV forEach loop can probably be added into the other forEach loops
-     */
+
     const content = fileReader.result;
 
-    const csv = content.split('\n');
-    const headers = csv.shift().split(',');
-    const table = [];
+    //Remove \r character that appears when file is parsed, then split rows on \n
+    const csv = content.replaceAll('\r', '').split('\n');
 
+    //First row is headers
+    const headers = csv.shift().split(',');
+
+    /**
+     * -Create return table
+     * -Add rows to return table, skipping empty rows
+     *
+     * TODO : this code can probably be cleaned up A LOT, but it is working
+     *    The first CSV forEach loop can probably be added into the other forEach loops
+     */
+    const table = [];
     csv.forEach((row) => {
       if(row === "") return; //skip row if row is empty, will continue parsing the file
       table.push(row.split(','));
