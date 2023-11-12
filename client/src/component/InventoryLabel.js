453b749688596acbc781e845f793a2448d7d2fa6
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-11 18:09:01 -0800

    Add hard corded label component that was used for testing

--- client/src/component/InventoryLabel.js
+++ client/src/component/InventoryLabel.js
@@ -109,6 +109,8 @@ class InventoryLabel extends Component {
     }
   
     render() {
+        //<Label make="Make Props" model="Model Props" serialNumber="Serial Number Props" assetNumber="123-Props" />
+
         //TODO : Make a clear button that clears state.labels variable
         return (<>
             { (this.props.batch === "false") && this.state.labels.length === 0 && <LabelForm setter={this.setLabels} />}
