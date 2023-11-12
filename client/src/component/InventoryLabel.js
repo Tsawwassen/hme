cf6d40505d3f0c4d30b0ff3ecd46d2337e5d904c
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-12 06:20:02 -0800

    Add react-bootstrap and useState modules

--- client/src/component/InventoryLabel.js
+++ client/src/component/InventoryLabel.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 import '../styles/InventoryLabel.scss';
 
