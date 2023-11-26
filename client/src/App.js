6bd0b43b98640b2bc9791d6286718a7797ff176c
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-11-26 07:36:27 -0800

    Add container around app components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
 
 import './App.css';
+
+//Modules
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//React-Bootstrap
+import Container from 'react-bootstrap/Container';
+
+//Components
+import Navigation from './component/Navigation.js';
 import Home from './component/Home.js';
 import Inventory from './component/Inventory.js';
 import StaleOrder from './component/StaleOrder.js';
 import Labels from './component/Labels.js';
 
-//Modules
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-//Components
-import Navigation from './component/Navigation.js';
-
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <BrowserRouter>
-        <Routes>
-            <Route path="home" element={<Home />} />
-            <Route path="inventory" element={<Inventory />} />
-            <Route path="staleOrder" element={<StaleOrder />} />
-            <Route path="labels" element={<Labels />} />
-        </Routes>
-    </BrowserRouter>
+      <Container>
+        <BrowserRouter>
+          <Routes>
+              <Route path="home" element={<Home />} />
+              <Route path="inventory" element={<Inventory />} />
+              <Route path="staleOrder" element={<StaleOrder />} />
+              <Route path="labels" element={<Labels />} />
+          </Routes>
+      </BrowserRouter>
+    </Container>
     </div>
   );
 }
