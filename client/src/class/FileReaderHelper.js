7d41ac7237eaa2139294133c217a5b60fb45effd
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-12-03 07:29:58 -0800

    Add funciton to parse WW export CSV file

--- client/src/class/FileReaderHelper.js
+++ client/src/class/FileReaderHelper.js
@@ -28,6 +28,44 @@ class FileReaderHelper {
             });
         });
         
+
+        return tempData;
+    }
+
+    static ParseWWCSV(content) {
+
+        //Remove \r character that appears when file is parsed, then split rows on \n
+        const csv = content.replaceAll('\r', '').split('\n');
+
+        //First row is headers
+        const headers = csv.shift().split(',');
+
+        /**
+         * -Create return table
+         * -Add rows to return table, skipping empty rows
+         *
+         * TODO : this code can probably be cleaned up A LOT, but it is working
+         *    The first CSV forEach loop can probably be added into the other forEach loops
+         */
+        console.log("ParseWWCSV");
+        debugger
+        const table = [];
+        csv.forEach((row) => {
+            if(row === "") return; //skip row if row is empty, will continue parsing the file
+            table.push(row.split(','));
+        });
+        
+        const tempData = []
+        table.forEach((row, i) => {
+            tempData[i] = {};
+            row.forEach((cell, j) =>{
+                tempData[i][headers[j]] =  cell;
+            });
+        });
+        
+        //console.log("inside FILEREADERHELPER.PARSECSV");
+        //debugger
+
         return tempData;
     }
 }
