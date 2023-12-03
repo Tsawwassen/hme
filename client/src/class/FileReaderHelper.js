23d852e29c6a8c7317fa8ce2e9113c96763d3c9d
Tsawwassen <mitchell.rian.smith@gmail.com>
2023-12-03 09:22:36 -0800

    Change CSV parsing function to use PapaParse to handle app breaking input data (comma, quote, empty lines)

--- client/src/class/FileReaderHelper.js
+++ client/src/class/FileReaderHelper.js
@@ -1,12 +1,33 @@
+import Papa from 'papaparse';
+
 class FileReaderHelper {
     static ParseCSV(content) {
 
+        let csv =  Papa.parse(content, {
+            header: true,
+            skipEmptyLines: 'greedy',
+            complete: function(results) {
+              return results.data;
+            }
+          });
+          
+          //The Papa.parse function would return an object. Needed to use a temp variable.data to return the actual file data to be used with the rest of the app
+          return csv.data;
+
+          /**
+           * DEV NOTE
+           *  PapaParse does all the parsing checks that the below code was doing (Except handeling commas)
+           *  use 'greedy' for skipEmptyLine to truely skip empty lines. Only having it as true would still keep empty cells
+           * Keeping the below code here for reference
+           */
+           
+          /** 
         //Remove \r character that appears when file is parsed, then split rows on \n
-        const csv = content.replaceAll('\r', '').split('\n');
+        const csv = content.replaceAll('\r', '').replaceAll('"', '').split('\n');
 
         //First row is headers
         const headers = csv.shift().split(',');
-
+*/
         /**
          * -Create return table
          * -Add rows to return table, skipping empty rows
@@ -14,6 +35,7 @@ class FileReaderHelper {
          * TODO : this code can probably be cleaned up A LOT, but it is working
          *    The first CSV forEach loop can probably be added into the other forEach loops
          */
+        /** 
         const table = [];
         csv.forEach((row) => {
             if(row === "") return; //skip row if row is empty, will continue parsing the file
@@ -28,10 +50,13 @@ class FileReaderHelper {
             });
         });
         
+        console.log("Inside FileReaderHelper.ParseCSV");
+        
 
-        return tempData;
+        return tempData;*/
     }
 
+    // DEV NOTE - Is this function needed anymore?
     static ParseWWCSV(content) {
 
         //Remove \r character that appears when file is parsed, then split rows on \n
