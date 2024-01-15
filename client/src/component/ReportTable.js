bb92c58d2e5af55cee0e60f669dcede11b41f820
Tsawwassen <mitchell.rian.smith@gmail.com>
2024-01-14 18:43:52 -0800

    Change report table to look like the WW export

--- client/src/component/ReportTable.js
+++ client/src/component/ReportTable.js
@@ -4,37 +4,41 @@
 import Table from 'react-bootstrap/Table';
 
 function ReportTable(props) {
+
     
     //Need to initialize keys variable to be used in the tbody render
     //Might be a better way of doing this, but not sure how to reference the keys array inside the map function
-    let keys = Object.keys(props.values);
+    
     return (
       <>
           <Table striped bordered hover>
             <thead>
             <tr>
-              <th>Inventory Description</th>
+              <th>Year</th>
               <th>Make</th>
               <th>Model</th>
               <th>Serial Number</th>
               <th>Unit Number</th>
+              <th>Inventory Desc</th>
+              <th>Department</th>
               <th>Expected</th>
               <th>Actual</th>
               <th>Difference</th>
             </tr>
             </thead>
             <tbody>
-            {keys.map((part, i) => {
+            {props.values.map((part, i) => {
               return <tr key={i}>
-                <td>PH- INVENTORY DESCRIPTION</td>
-                <td>PH- MAKE</td>
-                <td>PH- MODEL</td>
-                <td>PH- SERIAL NUMBER</td>
-                <td>{part}</td>
-                <td>{props.values[keys[i]].expected_qty}</td>
-                <td>{props.values[keys[i]].actual_qty}</td>
-                <td>{props.values[keys[i]].difference}</td>
-
+                <td>{part['Year']}</td>
+                <td>{part['Make']}</td>
+                <td>{part['Model']}</td>
+                <td>{part['Serial #']}</td>
+                <td>{part['(Unit No)']}</td>
+                <td>{part['Inventory Desc']}</td>
+                <td>{part['Department']}</td>
+                <td>{part['expected']}</td>
+                <td>{part['actual']}</td>
+                <td>{part['expected'] - part['actual']}</td>
               </tr>
             })}
             </tbody>
