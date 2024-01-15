a3f5ad7b7429ce64ee0a40eaca2aaaebcb810325
Tsawwassen <mitchell.rian.smith@gmail.com>
2024-01-14 18:43:12 -0800

    Change report state variable to array to work with WW export

--- client/src/component/Report.js
+++ client/src/component/Report.js
@@ -10,7 +10,7 @@ class Report extends Component {
     constructor(props){
         super(props);
         this.state = {
-            report: {}, 
+            report: [], 
             csvData: []
         };
 
