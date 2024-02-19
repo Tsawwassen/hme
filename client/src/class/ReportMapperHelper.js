9172bfd618c46f08105ab73e29010f0adbd14877
Tsawwassen <mitchell.rian.smith@gmail.com>
2024-02-19 13:19:06 -0800

    Update getDoubleFileContent to work with updated WW export and output report

--- client/src/class/ReportMapperHelper.js
+++ client/src/class/ReportMapperHelper.js
@@ -133,10 +133,22 @@ class ReportMapperHelper {
     // Get content from given files
     // TODO : now that the code is running asyncronously, need to show and catch better error messages. Keeping error message variables for future use.
     // Once both files are parsed, send them both to callback function
-    static async getDoubleFileContent(expectedPath, actualPath, callback, expectedErrorMessage, actualErrorMessage){ 
+    static async getDoubleFileContent(expectedPath, actualPath, callback){ 
+        //Need the actual file to look like scanned parts that is used inside getSingleFileContent
+        // // Scanned data is just an array of strings -> ['123', '123', '123']
+        // console.log(FileReaderHelper.ParseCSV(await this.readUploadedFileAsText(actualPath)))
+        // Current FileReaderHelper.ParseCSV is taking the first part as the column header
+        // Need to change an the papaparse to not check for headers, then figure out how to make it an array
+
+        //console.log(FileReaderHelper.ParseCSVNoHeader(await this.readUploadedFileAsText(actualPath)))
+        // setting header to false returns an array or arrays. with the given test file, each record has one value
+        //console.log(FileReaderHelper.ParseCSVNoHeader(await this.readUploadedFileAsText(actualPath)))
+        // Papaparse will always return a 2D array. Needed to use a mapping function to get the 2D array into a 1D array to be used by the report
+
+        
         callback(
-            FileReaderHelper.ParseCSV(await this.readUploadedFileAsText(expectedPath)),
-            FileReaderHelper.ParseCSV(await this.readUploadedFileAsText(actualPath))
+            this.formatWWData(FileReaderHelper.ParseCSV(await this.readUploadedFileAsText(expectedPath))),
+            FileReaderHelper.ParseCSVNoHeader(await this.readUploadedFileAsText(actualPath))
         )
      
     }
@@ -145,7 +157,7 @@ class ReportMapperHelper {
     // TODO : now that the code is running asyncronously, need to show and catch better error messages. Keeping error message variables for future use.
     // Once file is parsed, format scannedParts list to be same as expectedPath data, then send them both to callback function
     static async getSingleFileContent(expectedPath, scannedParts, callback){
-        
+       
         callback(
             this.formatWWData(FileReaderHelper.ParseCSV(await this.readUploadedFileAsText(expectedPath))),
             scannedParts
