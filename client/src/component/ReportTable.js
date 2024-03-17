5775117ee2e81fcd3c18316988861364f07aca07
Tsawwassen <mitchell.rian.smith@gmail.com>
2024-03-17 11:05:12 -0700

    Change report table to render data from new WW export

--- client/src/component/ReportTable.js
+++ client/src/component/ReportTable.js
@@ -8,17 +8,19 @@ function ReportTable(props) {
     
     //Need to initialize keys variable to be used in the tbody render
     //Might be a better way of doing this, but not sure how to reference the keys array inside the map function
+  
     
     return (
       <>
           <Table striped bordered hover>
             <thead>
             <tr>
-              <th>Department</th>
+              {/** <th>Department</th>  */}
               <th>Inventory Part</th>
-              <th>Make</th>
-              <th>Model</th>
-              <th>Inventory Desc</th>
+              {/** <th>Make</th>  */}
+              {/** <th>Model</th>  */}
+              {/** <th>Inventory Desc</th>  */}
+              <th>Unit Description</th>
               <th>Serial Number</th>
               <th>Unit Number</th>
               <th>WW Quantity</th>{/** Formerly Expected quantity */}
@@ -29,11 +31,12 @@ function ReportTable(props) {
             <tbody>
             {props.values.map((part, i) => {
               return <tr key={i}>
-                <td>{part['Department']}</td>
+                {/** <td>{part['Department']}</td> */}
                 <td>{part['Inventory Part']}</td>
-                <td>{part['Make']}</td>
-                <td>{part['Model']}</td>
-                <td>{part['Inventory Desc']}</td>
+                {/** <td>{part['Make']}</td> */}
+                {/** <td>{part['Model']}</td> */}
+                {/** <td>{part['Inventory Desc']}</td> */}
+                <td>{part['Unit Description']}</td>
                 <td>{part['Serial #']}</td>
                 <td>{part['(Unit No)']}</td>
                 <td>{part['expected']}</td>
