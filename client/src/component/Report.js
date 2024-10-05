18f55aa84f03ecc15ad576723c0041a3ba7d59ac
mitchell <mitchell.rian.smith@gmail.com>
2024-10-05 06:58:08 -0700

    Move the ' to the front of a cell that is only numbers. Also add this check to inventory part field

--- client/src/component/Report.js
+++ client/src/component/Report.js
@@ -45,16 +45,20 @@ class Report extends Component {
                 //Check if SN and Unit have value with only numbers
                 if(/^\d+$/.test(line["Serial #"])){
                     //add ` to the start of the number
-                    line["Serial #"] = line["Serial #"] + "`";
+                    line["Serial #"] = `'` + line["Serial #"] ;
                 }
             }
             if(line.hasOwnProperty("(Unit No)")){
                 //Check if SN and Unit have value with only numbers
                 if(/^\d+$/.test(line["(Unit No)"])){
                     //add ` to the start of the number
-                    line["(Unit No)"] = line["(Unit No)"]  + "`";
+                    line["(Unit No)"] = `'` + line["(Unit No)"];
                 }
             }
+            if(/^\d+(\.\d+)?$/.test(line["Inventory Part"])){
+                line["Inventory Part"] = `'` + line["Inventory Part"];
+            }
+             
             return line;
         })
         
