ca06ec11ba4d8989efae7bce7905d990df63889c
mitchell <mitchell.rian.smith@gmail.com>
2024-10-06 15:58:59 -0700

    Add shelf label single label option

--- client/src/component/Labels.js
+++ client/src/component/Labels.js
@@ -38,22 +38,24 @@ class Labels extends Component {
                     <Form.Label>Select Label to Print</Form.Label>
                     <Form.Select aria-label="Default select example" onChange={this.labelOnChange}>
                         <option value="0" >Select Label to Print</option>
-                        <option value="1" >Shelf Label Batch</option>
-                        <option value="2">Inventory Label Single</option>
-                        <option value="3">Inventory Label Batch</option>
-                        <option value="4">North Van - Inventory Label Single</option>
-                        <option value="5">North Van - Inventory Label Batch</option>
+                        <option value="1" >Shelf Label Single</option>
+                        <option value="2" >Shelf Label Batch</option>
+                        <option value="3">Inventory Label Single</option>
+                        <option value="4">Inventory Label Batch</option>
+                        <option value="5">North Van - Inventory Label Single</option>
+                        <option value="6">North Van - Inventory Label Batch</option>
                         {/**<option value="999">--TEST - Picture --</option>   Use this option for testing new labels */}
                     </Form.Select>
                 </Form.Group>
                 </Form>
             </div>
             {this.state.selectedLabelType === "0"}
-            {this.state.selectedLabelType === "1" && <ShelfLabel />}
-            {this.state.selectedLabelType === "2" && <InventoryLabel batch="false" />}
-            {this.state.selectedLabelType === "3" && <InventoryLabel batch="true" />}
-            {this.state.selectedLabelType === "4" && <InventoryLabelV2 batch="false" />}
-            {this.state.selectedLabelType === "5" && <InventoryLabelV2 batch="true" />}
+            {this.state.selectedLabelType === "1" && <ShelfLabel batch="false"/>}
+            {this.state.selectedLabelType === "2" && <ShelfLabel batch="true"/>}
+            {this.state.selectedLabelType === "3" && <InventoryLabel batch="false" />}
+            {this.state.selectedLabelType === "4" && <InventoryLabel batch="true" />}
+            {this.state.selectedLabelType === "5" && <InventoryLabelV2 batch="false" />}
+            {this.state.selectedLabelType === "6" && <InventoryLabelV2 batch="true" />}
             {/**{this.state.selectedLabelType === "999" && <InventoryLabel batch="false" imageTest="true" />} Use this option for testing new labels */}
         
         </>);
